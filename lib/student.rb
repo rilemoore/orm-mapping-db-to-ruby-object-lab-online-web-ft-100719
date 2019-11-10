633e339530b3c27e726d398badd3b6aec09d58e6
Riley Moore <rilemoore@gmail.com>
2019-11-10 04:16:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,6 +15,15 @@ class Student
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    
+    sql = "SELECT * FROM students"
+    row = DB[:conn].execute(sql).flatten
+    new_student = Student.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    row
+    
   end
 
   def self.find_by_name(name)
